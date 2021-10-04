3ec293c5b5c28372c781ac31191a4898fe280ccb
Tyler B <tylerwbennet@gmail.com>
2021-10-04 00:13:44 -0500

    Updating directory

--- Assignment4.4/src/main/java/com/bookclub/dao/BookDao.java
+++
@@ -1,8 +0,0 @@
-package com.bookclub.dao;
-
-import com.bookclub.model.Book;
-import com.bookclub.service.GenericDao;
-
-public interface BookDao extends GenericDao<Book, String> {
-    
-}
--- Assignment4.4/src/main/java/com/bookclub/service/GenericDao.java
+++
@@ -1,8 +0,0 @@
-package com.bookclub.service;
-
-import java.util.List;
-
-public interface GenericDao<E,K> {
-    public List<E> list(); //Return a list of objects of type E.
-    public E find(K key); //Return an object of type E by type K value.
-}
