44d1f317fab478721d66d6ddbaa322a501ebb483
twbennet-bellevue <twbennet@my.bellevue.edu>
2021-10-17 23:39:59 -0500

    Creating Assignment 7.2 Boilerplate

---
+++ Assignment7.2/src/main/java/com/bookclub/model/WishlistItem.java
@@ -0,0 +1,63 @@
+package com.bookclub.model;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "wishlist")
+public class WishlistItem {
+    
+    @Id
+    private String id;
+
+    @NotNull
+    @NotEmpty(message = "ISBN is a required field.")
+    private String isbn;
+    
+    @NotNull
+    @NotEmpty(message = "Title is a required field.")
+    private String title;
+
+    
+
+    public WishlistItem() {
+        this.isbn = "";
+        this.title = "";
+    }
+
+    public WishlistItem(String isbn, String title) {
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("WishlistItem{id=%s, isbn=%s, title=%s", id, isbn, title);
+    }
+
+}
