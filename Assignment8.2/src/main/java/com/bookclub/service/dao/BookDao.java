abe8664ceef953c504d55a512e466fa4883dfa5b
twbennet-bellevue <twbennet@my.bellevue.edu>
2021-10-21 03:15:48 -0500

    created assignment 8.2

---
+++ Assignment8.2/src/main/java/com/bookclub/service/dao/BookDao.java
@@ -0,0 +1,8 @@
+package com.bookclub.service.dao;
+
+import com.bookclub.model.Book;
+import com.bookclub.service.GenericDao;
+
+public interface BookDao extends GenericDao<Book, String> {
+    
+}
---
+++ Assignment8.2/src/main/java/com/bookclub/web/WishlistRestController.java
@@ -0,0 +1,38 @@
+package com.bookclub.web;
+
+import java.util.List;
+
+import com.bookclub.model.WishlistItem;
+import com.bookclub.service.dao.WishlistDao;
+import com.bookclub.service.impl.MongoWishlistDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api/wishlist", produces="application/json")
+@CrossOrigin(origins="*")
+public class WishlistRestController {
+
+    private WishlistDao wishlistDao = new MongoWishlistDao();
+
+    @Autowired
+    private void setWishlist(WishlistDao wishlistIn) {
+        this.wishlistDao = wishlistIn;
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    private List<WishlistItem> showWishlist() {
+        return wishlistDao.list();
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public WishlistItem findById(@PathVariable String id) {
+        return wishlistDao.find(id);
+    }
+}
